b05ad6124768f02cdf3820971fec9c9b56fab27a
varunmauraya2022 <varunmauraya@2022email.com>
2026-03-11 18:29:34 +0530

    dd ExpenseRepository using Spring Data JPA

---
+++ Backend/expanse-tracker-backend/src/main/java/com/amar/fullstack/expanse_tracker_backend/repository/ExpanseRepository.java
@@ -0,0 +1,9 @@
+package com.amar.fullstack.expanse_tracker_backend.repository;
+
+import com.amar.fullstack.expanse_tracker_backend.entity.Expanse;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExpanseRepository extends JpaRepository<Expanse, Long> {
+}
