bb007213305cb6cfde9579b6c26c476eb5ae82ad
varunmauraya2022 <varunmauraya@2022email.com>
2026-03-11 17:27:10 +0530

    Add Spring Boot backend project structure for ExpanseTracker

---
+++ Backend/expanse-tracker-backend/src/main/java/com/amar/fullstack/expanse_tracker_backend/ExpanseTrackerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.amar.fullstack.expanse_tracker_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpanseTrackerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpanseTrackerBackendApplication.class, args);
+	}
+
+}
---
+++ Backend/expanse-tracker-backend/src/test/java/com/amar/fullstack/expanse_tracker_backend/ExpanseTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amar.fullstack.expanse_tracker_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpanseTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
