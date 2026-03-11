4ab60bde7e97c7f993094dc320885fe65bc9fbc8
varunmauraya2022 <varunmauraya@2022email.com>
2026-03-11 19:44:19 +0530

    add ExpanseResponseDto to see responses with all fields

---
+++ Backend/expanse-tracker-backend/src/main/java/com/amar/fullstack/expanse_tracker_backend/dtos/ExpanseResponseDto.java
@@ -0,0 +1,73 @@
+package com.amar.fullstack.expanse_tracker_backend.dtos;
+
+import com.amar.fullstack.expanse_tracker_backend.entity.ExpanseCategory;
+
+import java.time.LocalDateTime;
+
+public class ExpanseResponseDto {
+
+    private Long id;
+    private String name;
+    private Double amount;
+    private ExpanseCategory category;
+    private String description;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public ExpanseCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(ExpanseCategory category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
+
