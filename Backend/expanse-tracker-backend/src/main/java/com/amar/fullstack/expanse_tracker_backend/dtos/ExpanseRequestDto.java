68f279fd41221ab4bffe9864526d3cd250b367c8
varunmauraya2022 <varunmauraya@2022email.com>
2026-03-11 19:40:13 +0530

    add ExpanseRequestDto for the sending request to create Expanse

---
+++ Backend/expanse-tracker-backend/src/main/java/com/amar/fullstack/expanse_tracker_backend/dtos/ExpanseRequestDto.java
@@ -0,0 +1,53 @@
+package com.amar.fullstack.expanse_tracker_backend.dtos;
+
+import com.amar.fullstack.expanse_tracker_backend.entity.ExpanseCategory;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public class ExpanseRequestDto {
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @NotNull(message = "Amount is required")
+    @Positive(message = "Amount must be positive")
+    private Double amount;
+
+    @NotBlank(message = "Category is required")
+    private ExpanseCategory category;
+
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
+
+    public ExpanseCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(ExpanseCategory category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
