789e10036cf20df78ef52a2af50106c0373dc478
varunmauraya2022 <varunmauraya@2022email.com>
2026-03-23 23:34:31 +0530

    add helper(dto) methods

--- Backend/expanse-tracker-backend/src/main/java/com/amar/fullstack/expanse_tracker_backend/service/ExpanseService.java
+++ Backend/expanse-tracker-backend/src/main/java/com/amar/fullstack/expanse_tracker_backend/service/ExpanseService.java
@@ -11,32 +11,46 @@ public class ExpanseService {
 
     private final ExpanseRepository expRepo;
 
-    public ExpanseService(ExpanseRepository expRepo){
-        this.expRepo=expRepo;
+    public ExpanseService(ExpanseRepository expRepo) {
+        this.expRepo = expRepo;
 
     }
 
-    public ExpanseResponseDto createExpanse(ExpanseRequestDto dto){
+    public ExpanseResponseDto createExpanse(ExpanseRequestDto dto) {
 
-        Expanse expanse=new Expanse();
+        Expanse expanse = new Expanse();
 
         expanse.setName(dto.getName());
         expanse.setAmount(dto.getAmount());
         expanse.setCategory(dto.getCategory());
         expanse.setDescription(dto.getDescription());
 
-        Expanse savedExpanse= expRepo.save(expanse);
+        Expanse savedExpanse = expRepo.save(expanse);
 
-        ExpanseResponseDto responseDto=new ExpanseResponseDto();
-        responseDto.setId(savedExpanse.getId());
-        responseDto.setName(savedExpanse.getName());
-        responseDto.setAmount(savedExpanse.getAmount());
-        responseDto.setCategory(savedExpanse.getCategory());
-        responseDto.setDescription(savedExpanse.getDescription());
-        responseDto.setCreatedAt(savedExpanse.getCreatedAt());
-        responseDto.setUpdatedAt(savedExpanse.getUpdatedAt());
+        return mapToResponse(savedExpanse);
+    }
 
+    private ExpanseResponseDto mapToResponse(Expanse expanse) {
+
+        ExpanseResponseDto responseDto = new ExpanseResponseDto();
+
+        responseDto.setId(expanse.getId());
+        responseDto.setName(expanse.getName());
+        responseDto.setAmount(expanse.getAmount());
+        responseDto.setCategory(expanse.getCategory());
+        responseDto.setCreatedAt(expanse.getCreatedAt());
+        responseDto.setUpdatedAt(expanse.getUpdatedAt());
         return responseDto;
     }
 
+    private Expanse mapToEntity(ExpanseRequestDto dto) {
+
+        Expanse expanse = new Expanse();
+
+        expanse.setName(dto.getName());
+        expanse.setAmount(dto.getAmount());
+        expanse.setCategory(dto.getCategory());
+        expanse.setCategory(dto.getCategory());
+        return expanse;
+    }
 }
