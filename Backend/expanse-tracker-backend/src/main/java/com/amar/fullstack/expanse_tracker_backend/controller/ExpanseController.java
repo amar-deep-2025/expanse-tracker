96992499c60d404a4f930fface0e8d57a3f4a1b0
varunmauraya2022 <varunmauraya@2022email.com>
2026-03-24 00:21:52 +0530

    add endpoint for findById

--- Backend/expanse-tracker-backend/src/main/java/com/amar/fullstack/expanse_tracker_backend/controller/ExpanseController.java
+++ Backend/expanse-tracker-backend/src/main/java/com/amar/fullstack/expanse_tracker_backend/controller/ExpanseController.java
@@ -2,11 +2,14 @@ package com.amar.fullstack.expanse_tracker_backend.controller;
 
 import com.amar.fullstack.expanse_tracker_backend.dtos.ExpanseRequestDto;
 import com.amar.fullstack.expanse_tracker_backend.dtos.ExpanseResponseDto;
+import com.amar.fullstack.expanse_tracker_backend.entity.Expanse;
 import com.amar.fullstack.expanse_tracker_backend.service.ExpanseService;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api/expanses")
@@ -14,17 +17,23 @@ public class ExpanseController {
 
     private final ExpanseService expService;
 
-    public ExpanseController(ExpanseService expService){
-        this.expService=expService;
+    public ExpanseController(ExpanseService expService) {
+        this.expService = expService;
     }
 
     @PostMapping()
-    public ExpanseResponseDto create(@Valid @RequestBody ExpanseRequestDto dto){
+    public ExpanseResponseDto create(@Valid @RequestBody ExpanseRequestDto dto) {
         return expService.createExpanse(dto);
     }
 
     @GetMapping()
-    public List<ExpanseResponseDto> findAll(){
+    public List<ExpanseResponseDto> findAll() {
         return expService.getAll(null);
+    }
+
+    @GetMapping("/{id}")
+    public ExpanseResponseDto getById(@PathVariable Long id) {
+        ExpanseResponseDto expanseResponseDto = expService.getById(id);
+        return expanseResponseDto;
     }
 }
