4a599e392760feb41cd4fe35b4e2b3b095de2a79
varunmauraya2022 <varunmauraya@2022email.com>
2026-03-24 14:07:25 +0530

    add CategoryRepsository

---
+++ Backend/expanse-tracker-backend/src/main/java/com/amar/fullstack/expanse_tracker_backend/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.amar.fullstack.expanse_tracker_backend.repository;
+import com.amar.fullstack.expanse_tracker_backend.entity.ExpanseCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CategoryRepository extends JpaRepository<ExpanseCategory,Long> {
+    Optional<ExpanseCategory> findByName(String name);
+}
