44b28192a39ddde2190979d825e3f9834ea3f14d
varunmauraya2022 <varunmauraya@2022email.com>
2026-04-01 22:12:07 +0530

    updated with findByEmail

--- Backend/expanse-tracker-backend/src/main/java/com/amar/fullstack/expanse_tracker_backend/repository/UserRepository.java
+++ Backend/expanse-tracker-backend/src/main/java/com/amar/fullstack/expanse_tracker_backend/repository/UserRepository.java
@@ -3,7 +3,9 @@ package com.amar.fullstack.expanse_tracker_backend.repository;
 import com.amar.fullstack.expanse_tracker_backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Long> {
 
-    User findByUsername(String username);
+    Optional<User> findByEmail(String email);
 }
