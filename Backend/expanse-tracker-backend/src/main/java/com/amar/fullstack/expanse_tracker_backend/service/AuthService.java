45e56894ce6820a73f9318d0e2e96cbbb487ff5e
varunmauraya2022 <varunmauraya@2022email.com>
2026-04-01 22:15:33 +0530

    updated AuthService with Optinal

---
+++ Backend/expanse-tracker-backend/src/main/java/com/amar/fullstack/expanse_tracker_backend/service/AuthService.java
@@ -0,0 +1,64 @@
+package com.amar.fullstack.expanse_tracker_backend.service;
+
+import com.amar.fullstack.expanse_tracker_backend.config.JwtUtil;
+import com.amar.fullstack.expanse_tracker_backend.dtos.AuthRequest;
+import com.amar.fullstack.expanse_tracker_backend.dtos.AuthResponse;
+import com.amar.fullstack.expanse_tracker_backend.dtos.RegisterRequest;
+import com.amar.fullstack.expanse_tracker_backend.entity.User;
+import com.amar.fullstack.expanse_tracker_backend.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class AuthService {
+
+    private final UserRepository userRepo;
+    private final PasswordEncoder passwordEncoder;
+    private JwtUtil jwtUtil;
+
+    public AuthService(UserRepository userRepo, PasswordEncoder passwordEncoder, JwtUtil jwtUtil) {
+        this.userRepo = userRepo;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtil = jwtUtil;
+    }
+
+    public void register(RegisterRequest request){
+
+        Optional<User> existUser = userRepo.findByEmail(request.getEmail());
+
+        if (existUser.isPresent()) {
+            throw new RuntimeException("User already exists with this email");
+        }
+
+        User user = new User();
+        user.setName(request.getName());
+        user.setEmail(request.getEmail());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setPhone(request.getPhone());
+
+        userRepo.save(user);
+    }
+
+    public AuthResponse login(AuthRequest request){
+
+        User user = userRepo.findByEmail(request.getEmail())
+                .orElseThrow(() -> new RuntimeException("Invalid email or password"));
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw new RuntimeException("Invalid email or password");
+        }
+
+        String token = jwtUtil.generateToken(user);
+
+        return new AuthResponse(
+                token,
+                user.getName(),
+                user.getEmail(),
+                user.getRole().name()
+        );
+    }
+
+
+}
