69ce4de1dc8524a59a71997d0f951f14cc785372
varunmauraya2022 <varunmauraya@2022email.com>
2026-04-01 22:07:59 +0530

    updated with validations

--- Backend/expanse-tracker-backend/src/main/java/com/amar/fullstack/expanse_tracker_backend/dtos/RegisterRequest.java
+++ Backend/expanse-tracker-backend/src/main/java/com/amar/fullstack/expanse_tracker_backend/dtos/RegisterRequest.java
@@ -1,11 +1,24 @@
 package com.amar.fullstack.expanse_tracker_backend.dtos;
 
+import jakarta.validation.constraints.*;
+
 public class RegisterRequest {
 
+    @NotBlank(message = "Name is required")
     private String name;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid Email Format")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min=5, message = "Password must be at least 5 characters")
     private String password;
+
+    @NotBlank(message = "Phone number is required")
+    @Pattern(regexp = "^\\d{10}$", message = "Phone number must be 10 digits")
     private String phone;
+
 
     public RegisterRequest(){}
 
