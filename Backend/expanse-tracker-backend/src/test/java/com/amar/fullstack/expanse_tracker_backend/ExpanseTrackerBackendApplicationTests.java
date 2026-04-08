524db46cf23b31dc555b4e0cf8f342da3b45522b
varunmauraya2022 <varunmauraya@2022email.com>
2026-04-08 17:05:18 +0530

    test: configure test class to use H2 database with test profile

--- Backend/expanse-tracker-backend/src/test/java/com/amar/fullstack/expanse_tracker_backend/ExpanseTrackerBackendApplicationTests.java
+++ Backend/expanse-tracker-backend/src/test/java/com/amar/fullstack/expanse_tracker_backend/ExpanseTrackerBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.amar.fullstack.expanse_tracker_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ExpanseTrackerBackendApplicationTests {
 
 	@Test
