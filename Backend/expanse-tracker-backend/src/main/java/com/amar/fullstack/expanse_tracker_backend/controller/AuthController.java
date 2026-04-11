89434298af461f45aabddf4d764b616e05a42213
varunmauraya2022 <varunmauraya@2022email.com>
2026-04-11 11:36:37 +0530

    feat(auth-controller): add structured logging for authentication APIs
    - Added INFO logs for register and login endpoints
    - Added DEBUG logs for request tracking (email only)
    - Improved API observability and removed console logs

--- Backend/expanse-tracker-backend/src/main/java/com/amar/fullstack/expanse_tracker_backend/controller/AuthController.java
+++ Backend/expanse-tracker-backend/src/main/java/com/amar/fullstack/expanse_tracker_backend/controller/AuthController.java
@@ -4,17 +4,17 @@ import com.amar.fullstack.expanse_tracker_backend.dtos.AuthRequest;
 import com.amar.fullstack.expanse_tracker_backend.dtos.AuthResponse;
 import com.amar.fullstack.expanse_tracker_backend.dtos.RegisterRequest;
 import com.amar.fullstack.expanse_tracker_backend.service.AuthService;
-import com.amar.fullstack.expanse_tracker_backend.service.UserService;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
 
     private final AuthService authService;
 
@@ -24,17 +24,20 @@ public class AuthController {
 
     @PostMapping("/register")
     public ResponseEntity<String> register(@Valid @RequestBody RegisterRequest request) {
-        // System.out.println("Name: " + request.getName());
-        // System.out.println("Email: " + request.getEmail());
-        // System.out.println("Password: " + request.getPassword());
-        // System.out.println("Phone: " + request.getPhone());
+
+        logger.info("Register API called");
+        logger.debug("Register request received for email: {}", request.getEmail());
         authService.register(request);
+        logger.info("User registered successfully");
         return ResponseEntity.ok("User registered successfully");
     }
 
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@Valid @RequestBody AuthRequest authRequest) {
+        logger.info("Login API called");
+        logger.debug("Login attempt for email: {}", authRequest.getEmail());
         AuthResponse response = authService.login(authRequest);
+        logger.info("User logged in successfully");
         return ResponseEntity.ok(response);
     }
 }
