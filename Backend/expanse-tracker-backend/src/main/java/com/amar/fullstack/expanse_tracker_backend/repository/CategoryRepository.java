2a9bc0bf9c4f5eff6732f721569febc1530eeaa1
varunmauraya2022 <varunmauraya@2022email.com>
2026-04-11 11:49:36 +0530

    fix(category-repo): fix category lookup to be case-insensitive

--- Backend/expanse-tracker-backend/src/main/java/com/amar/fullstack/expanse_tracker_backend/repository/CategoryRepository.java
+++ Backend/expanse-tracker-backend/src/main/java/com/amar/fullstack/expanse_tracker_backend/repository/CategoryRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<ExpanseCategory,Long> {
-    Optional<ExpanseCategory> findByName(String name);
+    Optional<ExpanseCategory> findByNameIgnoreCase(String name);
 }
