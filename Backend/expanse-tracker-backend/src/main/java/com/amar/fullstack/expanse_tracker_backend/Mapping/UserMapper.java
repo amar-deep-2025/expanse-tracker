4accbb79996cb97e892f05c422997efac67377e8
varunmauraya2022 <varunmauraya@2022email.com>
2026-04-12 20:52:35 +0530

    Add UserMapper for entity to DTO conversion

---
+++ Backend/expanse-tracker-backend/src/main/java/com/amar/fullstack/expanse_tracker_backend/Mapping/UserMapper.java
@@ -0,0 +1,19 @@
+package com.amar.fullstack.expanse_tracker_backend.Mapping;
+
+import com.amar.fullstack.expanse_tracker_backend.dtos.UserResponseDto;
+import com.amar.fullstack.expanse_tracker_backend.entity.User;
+
+public class UserMapper {
+
+    public static UserResponseDto toDto(User user){
+        return new UserResponseDto(
+                user.getName(),
+                user.getEmail(),
+                user.getPhone(),
+                user.getRole(),
+                user.getCreatedAt(),
+                user.getUpdatedAt(),
+                user.getProfileImage()
+        );
+    }
+}
