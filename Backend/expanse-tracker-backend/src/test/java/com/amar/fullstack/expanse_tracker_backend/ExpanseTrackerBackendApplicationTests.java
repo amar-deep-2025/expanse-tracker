a12db0c14baaaff43fffee4dcf9c80241de75ffd
varunmauraya2022 <varunmauraya@2022email.com>
2026-04-14 23:25:45 +0530

    Some code change but deleted

--- Backend/expanse-tracker-backend/src/test/java/com/amar/fullstack/expanse_tracker_backend/ExpanseTrackerBackendApplicationTests.java
+++ Backend/expanse-tracker-backend/src/test/java/com/amar/fullstack/expanse_tracker_backend/ExpanseTrackerBackendApplicationTests.java
@@ -2,7 +2,12 @@ package com.amar.fullstack.expanse_tracker_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.beans.Encoder;
+
 
 @SpringBootTest
 @ActiveProfiles("test")
