f33159303be637a3515bbb4d7c8d693f2a826b1c
varunmauraya2022 <varunmauraya@2022email.com>
2026-04-14 22:53:09 +0530

    feat: add custom queries in ExpanseRepository for totals, date filtering, monthly stats, and category-wise summaries

--- Backend/expanse-tracker-backend/src/main/java/com/amar/fullstack/expanse_tracker_backend/repository/ExpanseRepository.java
+++ Backend/expanse-tracker-backend/src/main/java/com/amar/fullstack/expanse_tracker_backend/repository/ExpanseRepository.java
@@ -1,4 +1,5 @@
 package com.amar.fullstack.expanse_tracker_backend.repository;
+
 import com.amar.fullstack.expanse_tracker_backend.entity.Expanse;
 import com.amar.fullstack.expanse_tracker_backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,34 +12,54 @@ import java.util.List;
 @Repository
 public interface ExpanseRepository extends JpaRepository<Expanse, Long> {
 
-    List<Expanse> findByUserId(Long userId);
+        List<Expanse> findByUser_Id(Long userId);
 
-    @Query("SELECT SUM(e.amount) FROM Expanse e WHERE e.user = :user AND e.type = 'EXPENSE'")
-    Double getTotalExpense(User user);
+        @Query("SELECT SUM(e.amount) FROM Expanse e WHERE e.user.id = :userId AND e.type = 'EXPENSE'")
+        Double getTotalExpense(Long userId);
 
-    @Query("SELECT SUM(e.amount) FROM Expanse e WHERE e.user = :user AND e.type = 'INCOME'")
-    Double getTotalIncome(User user);
+        @Query("SELECT SUM(e.amount) FROM Expanse e WHERE e.user.id = :userId AND e.type = 'INCOME'")
+        Double getTotalIncome(Long userId);
 
-    @Query("SELECT SUM(e.amount) FROM Expanse e WHERE e.user = :user AND e.type = 'EXPENSE' AND e.expanseDate BETWEEN :startDate AND :endDate")
-    Double getExpenseBetweenDates(User user, LocalDateTime startDate, LocalDateTime endDate);
+        @Query("SELECT SUM(e.amount) FROM Expanse e WHERE e.user.id = :userId AND e.type = 'EXPENSE' AND e.expanseDate BETWEEN :startDate AND :endDate")
+        Double getExpenseBetweenDates(Long userId, LocalDateTime startDate, LocalDateTime endDate);
 
-    @Query("SELECT e.category.name, SUM(e.amount) FROM Expanse e WHERE e.user = :user AND e.type = 'EXPENSE' AND e.expanseDate BETWEEN :startDate AND :endDate GROUP BY e.category.name")
-    List<Object[]> getCategorySummary(User user, LocalDateTime startDate, LocalDateTime endDate);
+        @Query("SELECT e.category.name, SUM(e.amount) FROM Expanse e WHERE e.user.id = :userId AND e.type = 'EXPENSE' AND e.expanseDate BETWEEN :startDate AND :endDate GROUP BY e.category.name")
+        List<Object[]> getCategorySummary(Long userId, LocalDateTime startDate, LocalDateTime endDate);
 
-    List<Expanse> findTop5ByUserOrderByExpanseDateDesc(User user);
+        List<Expanse> findTop5ByUser_IdOrderByExpanseDateDesc(Long userId);
 
-    @Query("SELECT SUM(e.amount) FROM Expanse e WHERE e.user = :user AND e.type = INCOME AND e.expanseDate BETWEEN :startDate AND :endDate")
-    Double getIncomeBetweenDates(User user, LocalDateTime startDate, LocalDateTime endDate);
+        @Query("SELECT SUM(e.amount) FROM Expanse e WHERE e.user.id = :userId AND e.type = 'INCOME' AND e.expanseDate BETWEEN :startDate AND :endDate")
+        Double getIncomeBetweenDates(Long userId, LocalDateTime startDate, LocalDateTime endDate);
 
-    @Query("""
-          SELECT 
-          MONTH(e.expanseDate),
-          SUM(CASE WHEN e.type = 'INCOME' THEN e.amount ELSE 0 END),
-          SUM(CASE WHEN e.type = 'EXPENSE' THEN e.amount ELSE 0 END)
-          FROM Expanse e
-          WHERE e.user = :user 
-          AND YEAR(e.expanseDate) = :year
-          GROUP BY MONTH(e.expanseDate)
-          ORDER BY MONTH(e.expanseDate)""")
-          List<Object[]> getMonthlyIncomeExpense(User user, int year);
+        @Query("""
+                        SELECT
+                        MONTH(e.expanseDate),
+                        SUM(CASE WHEN e.type = 'INCOME' THEN e.amount ELSE 0 END),
+                        SUM(CASE WHEN e.type = 'EXPENSE' THEN e.amount ELSE 0 END)
+                        FROM Expanse e
+                        WHERE e.user.id = :userId
+                        AND YEAR(e.expanseDate) = :year
+                        GROUP BY MONTH(e.expanseDate)
+                        ORDER BY MONTH(e.expanseDate)
+                        """)
+        List<Object[]> getMonthlyIncomeExpense(Long userId, int year);
+
+        @Query("""
+                        SELECT SUM(e.amount) FROM Expanse e
+                        WHERE e.user.id = :userId
+                        AND e.type='EXPENSE'
+                        AND MONTH(e.expanseDate)=:month
+                        AND YEAR(e.expanseDate)=:year
+                        """)
+        Double getExpenseByMonth(Long userId, int month, int year);
+
+        @Query("""
+                        SELECT c.name, SUM(e.amount) FROM Expanse e
+                        JOIN e.category c
+                        WHERE e.user.id = :userId
+                        AND e.type='EXPENSE'
+                        GROUP BY c.name
+                        ORDER BY SUM(e.amount) DESC
+                        """)
+        List<Object[]> getTopCategory(Long userId);
 }
