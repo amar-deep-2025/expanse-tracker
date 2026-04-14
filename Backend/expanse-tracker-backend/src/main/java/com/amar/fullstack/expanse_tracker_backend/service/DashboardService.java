aca6efa4d9084fb269a593b41e4f37392479555f
varunmauraya2022 <varunmauraya@2022email.com>
2026-04-14 23:00:51 +0530

    feat: add advanced analytics in DashboardService including monthly comparison and top category insights

--- Backend/expanse-tracker-backend/src/main/java/com/amar/fullstack/expanse_tracker_backend/service/DashboardService.java
+++ Backend/expanse-tracker-backend/src/main/java/com/amar/fullstack/expanse_tracker_backend/service/DashboardService.java
@@ -1,14 +1,13 @@
 package com.amar.fullstack.expanse_tracker_backend.service;
 
-import com.amar.fullstack.expanse_tracker_backend.dtos.CategoryDto;
-import com.amar.fullstack.expanse_tracker_backend.dtos.DashboardSummaryDto;
-import com.amar.fullstack.expanse_tracker_backend.dtos.MonthlyDto;
+import com.amar.fullstack.expanse_tracker_backend.dtos.*;
 import com.amar.fullstack.expanse_tracker_backend.entity.Expanse;
 import com.amar.fullstack.expanse_tracker_backend.entity.User;
 import com.amar.fullstack.expanse_tracker_backend.repository.ExpanseRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -27,65 +26,118 @@ public class DashboardService {
 
     public DashboardSummaryDto getSummary(User user) {
 
-        logger.info("Calculating dashboard summary for user: {}", user.getEmail());
+        Long userId = user.getId();
 
-        Double expense = defaultZero(expRepo.getTotalExpense(user));
-        Double income = defaultZero(expRepo.getTotalIncome(user));
-        double balance = income - expense;
-        logger.debug("Summary -> income: {}, expense: {}, balance: {}",
-                income, expense, balance);
-        return new DashboardSummaryDto(expense, income, balance);
+        Double expense = defaultZero(expRepo.getTotalExpense(userId));
+        Double income = defaultZero(expRepo.getTotalIncome(userId));
+
+        return new DashboardSummaryDto(expense, income, income - expense);
     }
 
     public DashboardSummaryDto getSummaryByDate(
             User user,
             LocalDateTime start,
             LocalDateTime end) {
-        logger.info("Calculating summary by date for user: {} from {} to {}",
-                user.getEmail(), start, end);
+
+        Long userId = user.getId();
+
         Double income = defaultZero(
-                expRepo.getIncomeBetweenDates(user, start, end));
+                expRepo.getIncomeBetweenDates(userId, start, end));
+
         Double expense = defaultZero(
-                expRepo.getExpenseBetweenDates(user, start, end));
-        double balance = income - expense;
-        logger.debug("Date Summary -> income: {}, expense: {}, balance: {}",
-                income, expense, balance);
-        return new DashboardSummaryDto(expense, income, balance);
+                expRepo.getExpenseBetweenDates(userId, start, end));
+
+        return new DashboardSummaryDto(expense, income, income - expense);
     }
 
     public List<CategoryDto> getCategorySummary(
             User user,
             LocalDateTime start,
             LocalDateTime end) {
-        logger.info("Fetching category summary for user: {} between {} and {}",
-                user.getEmail(), start, end);
-        List<Object[]> data = expRepo.getCategorySummary(user, start, end);
-        logger.debug("Raw category data size: {}", data.size());
-        return data.stream()
+
+        Long userId = user.getId();
+
+        return expRepo.getCategorySummary(userId, start, end)
+                .stream()
                 .map(this::mapToCategoryDto)
                 .toList();
     }
 
     public List<MonthlyDto> getMonthly(User user, int year) {
 
-        logger.info("Fetching monthly data for user: {} for year: {}",
-                user.getEmail(), year);
-        List<Object[]> data = expRepo.getMonthlyIncomeExpense(user, year);
-        logger.debug("Monthly raw data size: {}", data.size());
-        return data.stream()
+        Long userId = user.getId();
+
+        return expRepo.getMonthlyIncomeExpense(userId, year)
+                .stream()
                 .map(this::mapToMonthlyDto)
                 .toList();
     }
 
-    public List<Expanse> getRecentExpenses(User user) {
-        logger.info("Fetching recent expenses for user: {}", user.getEmail());
-        List<Expanse> expenses =
-                expRepo.findTop5ByUserOrderByExpanseDateDesc(user);
+    public List<RecentExpanseDto> getRecentExpenses(User user) {
 
-        logger.debug("Fetched {} recent expenses", expenses.size());
-
-        return expenses;
+        return expRepo.findTop5ByUser_IdOrderByExpanseDateDesc(user.getId())
+                .stream()
+                .map(e -> new RecentExpanseDto(
+                        e.getId(),
+                        e.getName(),
+                        e.getAmount(),
+                        e.getCategory().getName(),
+                        e.getType().name()
+                ))
+                .toList();
     }
+    public ComparisonDto compareCurrentMonth(Long userId){
+
+        LocalDateTime now = LocalDateTime.now();
+
+        int currentMonth = now.getMonthValue();
+        int currentYear = now.getYear();
+
+        LocalDateTime lastMonthDate = now.minusMonths(1);
+
+        int lastMonth = lastMonthDate.getMonthValue();
+        int lastYear = lastMonthDate.getYear();
+
+        double current = defaultZero(
+                expRepo.getExpenseByMonth(userId, currentMonth, currentYear)
+        );
+
+        double last = defaultZero(
+                expRepo.getExpenseByMonth(userId, lastMonth, lastYear)
+        );
+
+        double diff = current - last;
+
+        double percent = last == 0 ? 100 : (diff / last) * 100;
+        percent = Math.round(percent * 100.0) / 100.0;
+
+        String status = diff > 0 ? "Increased"
+                : diff < 0 ? "Decreased" : "No Change";
+
+        return new ComparisonDto(
+                current, last,
+                Math.abs(diff),
+                Math.abs(percent),
+                status
+        );
+    }
+
+    public CategoryDto getTopCategory(Long userId){
+
+        List<Object[]> data = expRepo.getTopCategory(userId);
+
+        if (data.isEmpty()){
+            return new CategoryDto("No Data", 0.0);
+        }
+
+        Object[] row = data.get(0);
+
+        return new CategoryDto(
+                (String) row[0],
+                row[1] != null ? ((Number) row[1]).doubleValue() : 0.0
+        );
+    }
+
 
     private Double defaultZero(Double value) {
         return value == null ? 0.0 : value;
@@ -99,13 +151,10 @@ public class DashboardService {
     }
 
     private MonthlyDto mapToMonthlyDto(Object[] obj) {
-        Integer month = (Integer) obj[0];
-        double income = obj[1] != null
-                ? ((Number) obj[1]).doubleValue()
-                : 0.0;
-        double expense = obj[2] != null
-                ? ((Number) obj[2]).doubleValue()
-                : 0.0;
-        return new MonthlyDto(month, income, expense);
+        return new MonthlyDto(
+                (Integer) obj[0],
+                obj[1] != null ? ((Number) obj[1]).doubleValue() : 0.0,
+                obj[2] != null ? ((Number) obj[2]).doubleValue() : 0.0
+        );
     }
 }
