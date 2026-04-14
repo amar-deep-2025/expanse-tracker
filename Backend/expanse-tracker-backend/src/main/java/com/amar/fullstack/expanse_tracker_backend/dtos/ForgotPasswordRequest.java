a82d45fcbaa9f9bda163cfb95305389562a4beff
varunmauraya2022 <varunmauraya@2022email.com>
2026-04-14 22:47:27 +0530

    add:feat forgotPasswordRequest

---
+++ Backend/expanse-tracker-backend/src/main/java/com/amar/fullstack/expanse_tracker_backend/dtos/ForgotPasswordRequest.java
@@ -0,0 +1,14 @@
+package com.amar.fullstack.expanse_tracker_backend.dtos;
+
+public class ForgotPasswordRequest {
+
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
