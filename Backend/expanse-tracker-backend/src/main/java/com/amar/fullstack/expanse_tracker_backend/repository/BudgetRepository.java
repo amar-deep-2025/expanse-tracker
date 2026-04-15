b9d6fe2edde16907943a5f18d47aab884769dc79
varunmauraya2022 <varunmauraya@2022email.com>
2026-04-15 19:08:31 +0530

    enhance feat: Budget repository for the dashboard pages

--- Backend/expanse-tracker-backend/src/main/java/com/amar/fullstack/expanse_tracker_backend/repository/BudgetRepository.java
+++ Backend/expanse-tracker-backend/src/main/java/com/amar/fullstack/expanse_tracker_backend/repository/BudgetRepository.java
@@ -3,8 +3,10 @@ package com.amar.fullstack.expanse_tracker_backend.repository;
 import com.amar.fullstack.expanse_tracker_backend.entity.Budget;
 import com.amar.fullstack.expanse_tracker_backend.entity.BudgetType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,4 +31,35 @@ public interface BudgetRepository extends JpaRepository<Budget,Long> {
             BudgetType type,
             Long categoryId
     );
+
+    @Query("SELECT COALESCE(SUM(b.budget), 0) FROM Budget b WHERE b.user.id = :userId")
+    Double getTotalBudget(Long userId);
+
+    @Query("""
+       SELECT SUM(b.budget) FROM Budget b
+    WHERE b.user.id = :userId
+    AND b.month = :month
+    AND b.year = :year
+""")
+    Double getTotalBudgetByMonth(Long userId, Integer month, Integer year);
+
+    @Query("""
+         SELECT COALESCE(SUM(b.budget), 0) FROM Budget b
+        WHERE b.user.id = :userId
+        AND (
+        (b.year > :startYear OR (b.year = :startYear AND b.month >= :startMonth))
+        AND
+        (b.year < :endYear OR (b.year = :endYear AND b.month <= :endMonth))
+        )
+     """)
+    Double getBudgetBetweenMonths(
+            Long userId,
+            Integer startMonth,
+            Integer startYear,
+            Integer endMonth,
+            Integer endYear
+    );
+
+
+
 }
