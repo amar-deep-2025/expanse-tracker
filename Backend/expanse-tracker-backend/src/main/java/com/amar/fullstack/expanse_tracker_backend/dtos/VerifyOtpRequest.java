62314ebd5a481c96457140b8d4c32196486de74f
varunmauraya2022 <varunmauraya@2022email.com>
2026-04-23 00:20:03 +0530

    feat(auth): introduce VerifyOtpRequest DTO to handle OTP verification during authentication and password reset

---
+++ Backend/expanse-tracker-backend/src/main/java/com/amar/fullstack/expanse_tracker_backend/dtos/VerifyOtpRequest.java
@@ -0,0 +1,24 @@
+package com.amar.fullstack.expanse_tracker_backend.dtos;
+
+
+public class VerifyOtpRequest {
+
+    private String email;
+    private String otp;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getOtp() {
+        return otp;
+    }
+
+    public void setOtp(String otp) {
+        this.otp = otp;
+    }
+}
