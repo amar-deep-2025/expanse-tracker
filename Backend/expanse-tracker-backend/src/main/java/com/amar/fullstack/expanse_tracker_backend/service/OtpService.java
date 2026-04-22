a25fbcc83fcb7648527bfef072d65f19f1a9a46e
varunmauraya2022 <varunmauraya@2022email.com>
2026-04-23 00:35:50 +0530

    feat(auth): add OtpService for OTP generation and storage using Redis

---
+++ Backend/expanse-tracker-backend/src/main/java/com/amar/fullstack/expanse_tracker_backend/service/OtpService.java
@@ -0,0 +1,49 @@
+package com.amar.fullstack.expanse_tracker_backend.service;
+
+import com.amar.fullstack.expanse_tracker_backend.dtos.RegisterRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class OtpService {
+
+    @Autowired
+    private StringRedisTemplate  redisTemplate;
+
+    public String generateOtp(){
+        return String.valueOf(new Random().nextInt(900000)+100000);
+    }
+
+    public void saveOtp(String email, String otp){
+        redisTemplate.opsForValue().set(email, otp,5, TimeUnit.MINUTES);
+
+    }
+
+    public String getOtp(String email){
+        return redisTemplate.opsForValue().get(email);
+    }
+
+    public void deleteOtp(String email){
+        redisTemplate.delete("OTP:"+email);
+    }
+
+    public void sendOtp(String email, String otp){
+        System.out.println("OTP"+otp);
+    }
+
+    public void saveUserData(RegisterRequest request){
+        String key="USER:"+request.getEmail();
+
+        String value=request.getName()+","+
+                request.getEmail()+","+
+                request.getPassword()+","+
+                request.getPhone();
+
+        redisTemplate.opsForValue().set(key,value,5,TimeUnit.MINUTES);
+    }
+}
