1d9f6966a6c2da0185ee184c577a39b7edd750a7
varunmauraya2022 <varunmauraya@2022email.com>
2026-04-23 00:44:10 +0530

    add annotation to enabling scheduling

--- Backend/expanse-tracker-backend/src/main/java/com/amar/fullstack/expanse_tracker_backend/ExpanseTrackerBackendApplication.java
+++ Backend/expanse-tracker-backend/src/main/java/com/amar/fullstack/expanse_tracker_backend/ExpanseTrackerBackendApplication.java
@@ -2,8 +2,10 @@ package com.amar.fullstack.expanse_tracker_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ExpanseTrackerBackendApplication {
 
 	public static void main(String[] args) {
