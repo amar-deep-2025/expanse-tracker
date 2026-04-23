3cc31d0daa371055777f606cbfb0376f7be74ce4
varunmauraya2022 <varunmauraya@2022email.com>
2026-04-23 22:26:06 +0530

    remove unused imports

--- Backend/expanse-tracker-backend/src/test/java/com/amar/fullstack/expanse_tracker_backend/ExpanseTrackerBackendApplicationTests.java
+++ Backend/expanse-tracker-backend/src/test/java/com/amar/fullstack/expanse_tracker_backend/ExpanseTrackerBackendApplicationTests.java
@@ -1,14 +1,6 @@
 package com.amar.fullstack.expanse_tracker_backend;
-
-import com.amar.fullstack.expanse_tracker_backend.notification.strategy.EmailNotificationStrategy;
-import com.amar.fullstack.expanse_tracker_backend.notification.strategy.SmsNotificationStrategy;
-import com.amar.fullstack.expanse_tracker_backend.service.BudgetService;
-import com.amar.fullstack.expanse_tracker_backend.service.DashboardService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
