1293518069e7b7f613b81de4bec11650a0939921
varunmauraya2022 <varunmauraya@2022email.com>
2026-04-23 22:30:04 +0530

    try to fixed test

--- Backend/expanse-tracker-backend/src/test/java/com/amar/fullstack/expanse_tracker_backend/ExpanseTrackerBackendApplicationTests.java
+++ Backend/expanse-tracker-backend/src/test/java/com/amar/fullstack/expanse_tracker_backend/ExpanseTrackerBackendApplicationTests.java
@@ -1,11 +1,29 @@
 package com.amar.fullstack.expanse_tracker_backend;
+
+import com.amar.fullstack.expanse_tracker_backend.notification.strategy.EmailNotificationStrategy;
+import com.amar.fullstack.expanse_tracker_backend.notification.strategy.SmsNotificationStrategy;
+import com.amar.fullstack.expanse_tracker_backend.service.OtpService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class ExpanseTrackerBackendApplicationTests {
+
+	@MockBean
+	private OtpService otpService;
+
+	@MockBean
+	private EmailNotificationStrategy emailNotificationStrategy;
+
+	@MockBean
+	private SmsNotificationStrategy smsNotificationStrategy;
+
+	@MockBean
+	private StringRedisTemplate stringRedisTemplate;
 
 	@Test
 	void contextLoads() {
