ed6da0eebc36f7fa1da6cb9db60f643f23b05f61
varunmauraya2022 <varunmauraya@2022email.com>
2026-04-24 22:53:52 +0530

    feat: configure Razorpay client with environment variables

--- Backend/expanse-tracker-backend/src/main/java/com/amar/fullstack/expanse_tracker_backend/config/RazorpayConfig.java
+++ Backend/expanse-tracker-backend/src/main/java/com/amar/fullstack/expanse_tracker_backend/config/RazorpayConfig.java
@@ -1,4 +1,29 @@
 package com.amar.fullstack.expanse_tracker_backend.config;
 
-public class RozarPayConfig {
+
+import com.razorpay.RazorpayClient;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RazorpayConfig {
+
+    @Value("${razorpay.key.id}")
+    private String keyId;
+
+    @Value("${razorpay.key.secret}")
+    private String keySecret;
+
+    @Bean
+    public RazorpayClient razorpayClient() throws Exception{
+        return new RazorpayClient(keyId,keySecret);
+
+    }
+    @PostConstruct
+    public void testKeys() {
+        System.out.println("KEY_ID = " +keyId);
+        System.out.println("KEY_SECRET = " +keySecret);
+    }
 }
